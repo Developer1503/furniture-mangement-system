d3ae7acbb109edaf1b648d43f21deed4692d779e
Developer1503 <vedantshinde305@gmail.com>
2024-12-30 01:12:12 +0530

    LatestCollection added using hooks

--- frontend/src/components/Productitem.jsx
+++ frontend/src/components/Productitem.jsx
@@ -1,22 +1,19 @@
-import React, { useContext } from 'react'
-import { ShopContext } from '../context/ShopContext'
+import React, { useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
 import { Link } from 'react-router-dom';
 
-const Productitem = ({id,image,name,price}) => {
-const {currency} = useContext(ShopContext);
-
+const Productitem = ({ id, image, name, price }) => {
+  const { currency } = useContext(ShopContext);
 
   return (
     <Link className='text-gray-500 cursor-pointer' to={`/product/${id}`}>
-    <div className='overflow-hidden'>
-    <img className='hover:scale-105 transition ease-in-out' src={image[0]} alt='' />
-    </div>
-    <p className='pt-3 pb-1 text-sm'>{name}</p>
-    <p className='text-sm font-medium'>{currency}{price}</p>
-
-      
+      <div className='overflow-hidden rounded-lg shadow-lg'>
+        <img className='transition ease-in-out duration-300 transform hover:scale-105' src={image[0]} alt={name} />
+      </div>
+      <p className='pt-3 pb-1 text-sm'>{name}</p>
+      <p className='text-sm font-medium'>{currency}{price}</p>
     </Link>
-  )
+  );
 }
 
-export default Productitem
+export default Productitem;
--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const Title = ({text1,text2}) => {
+const Title = ({ text1, text2 }) => {
   return (
     <div className='inline-flex gap-2 items-center mb-3'>
-    <p className='text-gray-900'>{text1} <span className='text-gray-500 font-medium'>{text2}</span></p>
-    <p className='w-8 sm:w-12 h-[1px] sm:h[2px] bg-gray-700'></p>
-
-      
+      <p className='font-roboto-bold text-4xl'>
+        {text1} <span className='text-gray-500 font-medium'>{text2}</span>
+      </p>
+      <p className='w-8 sm:w-12 h-[1px] sm:h-[2px] bg-gray-700'></p>
     </div>
-  )
+  );
 }
 
-export default Title
+export default Title;
