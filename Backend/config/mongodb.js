8d5f8ea63151b88a321ee1efc1ff42983ae60770
Rithesh14 <167207142+Rithesh14@users.noreply.github.com>
2025-01-15 22:03:43 +0530

    mongodb.js

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -1,12 +1,13 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
-
-  mongoose.connection.on("connected", () => {
-   console.log("MongoDB connected");  
-  })
-
-  await mongoose.connect(`${process.env.MONGODB_URI}/antro`)
-}
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
 
 export default connectDB;
