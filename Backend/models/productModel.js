eb7539daedadc11655e0596ce665796a6e4d050d
Rithesh14 <167207142+Rithesh14@users.noreply.github.com>
2025-01-23 19:20:59 +0530

    productModel.js

--- Backend/models/productModel.js
+++ Backend/models/productModel.js
@@ -6,8 +6,6 @@ const productSchema = new mongoose.Schema({
   price: {type:Number, required: true},
   image: {type:Array, required: true},
   category: {type:String, required: true},
-  subCategory: {type:String, required: true},
-  sizes: {type:Array, required: true},
   bestseller: {type:Boolean},
   date: {type:Date, required: true},
 })
